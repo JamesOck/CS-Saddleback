b5d87ce68d6d349a72348e31a08e98ada2f35fa5
JamesOck <157070839+JamesOck@users.noreply.github.com>
2024-12-15 22:50:44 -0800

    Update CS1C_Library_item.h

--- CS1C_Library_item.h
+++ CS1C_Library_item.h
@@ -57,73 +57,7 @@ class Magazine : public Item {
 public:
     Magazine(string name, string description, int id, string edition, string mainArticleTitle);
     void print(ostream &out) const override;
-}
-
-// Helper function to clear input buffer
-void clearInput() {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-
-// Function to get item details from the user
-Item* createItem() {
-    int itemType;
-    cout << "Select Item Type:\n1. Book\n2. Movie\n3. Magazine\n";
-    cin >> itemType;
-    clearInput();
-
-    string name, description;
-    int id;
-    cout << "Enter Item Name: ";
-    getline(cin, name);
-    cout << "Enter Item Description: ";
-    getline(cin, description);
-    cout << "Enter Item ID: ";
-    cin >> id;
-    clearInput();
-
-    if (itemType == 1) {
-        string title, author;
-        int copyrightDate;
-        cout << "Enter Book Title: ";
-        getline(cin, title);
-        cout << "Enter Author: ";
-        getline(cin, author);
-        cout << "Enter Copyright Date: ";
-        cin >> copyrightDate;
-        clearInput();
-        return new Book(name, description, id, title, author, copyrightDate);
-    } else if (itemType == 2) {
-        string title, director;
-        vector<string> mainActors;
-        cout << "Enter Movie Title: ";
-        getline(cin, title);
-        cout << "Enter Director: ";
-        getline(cin, director);
-        cout << "Enter Main Actors (comma-separated): ";
-        string actorsInput;
-        getline(cin, actorsInput);
-
-        size_t start = 0, end;
-        while ((end = actorsInput.find(',', start)) != string::npos) {
-            mainActors.push_back(actorsInput.substr(start, end - start));
-            start = end + 1;
-        }
-        mainActors.push_back(actorsInput.substr(start));
-        return new Movie(name, description, id, title, director, mainActors);
-    } else if (itemType == 3) {
-        string edition, mainArticle;
-        cout << "Enter Magazine Edition: ";
-        getline(cin, edition);
-        cout << "Enter Title of Main Article: ";
-        getline(cin, mainArticle);
-        return new Magazine(name, description, id, edition, mainArticle);
-    } else {
-        cout << "Invalid item type. Returning null.\n";
-        return nullptr;
-    }
 };
-
 
 
 #endif // ITEM_H
